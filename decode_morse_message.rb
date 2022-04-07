382cba0a45eb08e539653196581f6721da10af78
tresorsawasawa <tresorsawasawa@gmail.com>
2022-04-07 10:13:22 +0200

    Add methods to decode morse messsges and for spces between words

--- decode_morse_message.rb
+++ decode_morse_message.rb
@@ -1,6 +1,26 @@
-@decoder = {'.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D' , '.' => 'E', '..-.' => 'F', '--.' => 'G', 
+def decode_char(char)
+  decoder = {'.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D' , '.' => 'E', '..-.' => 'F', '--.' => 'G', 
     '....' => 'H', '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O', 
     '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T', '..-' => 'U', '...-' => 'V', '.--' => 'W', 
     '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z'}
 
-    
+  decoder[char]
+end
+
+def separator(word)
+  word.split('   ')
+end
+
+def decode_msg(message)
+  result = ''
+  message.each do |n|
+    word = n.split(' ')
+    word.each do |m|
+      result += decode_char(m)
+    end
+    result += ' '
+  end
+  puts(result)
+end
+
+decode_msg(separator('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'))
